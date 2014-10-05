cbbe71ae2beecda2f8fdaedf8910243d973bd12b
Chao Chen <roychen92@163.com>
2014-10-05 08:31:19 +0800

    add SetCard class

---
+++ Matchismo/SetCard.h
@@ -0,0 +1,18 @@
+//
+//  SetCard.h
+//  Matchismo
+//
+//  Created by 陈超 on 14-10-5.
+//  Copyright (c) 2014年 Chao Chen. All rights reserved.
+//
+
+#import "Card.h"
+
+@interface SetCard : Card
+
+@property (nonatomic) NSUInteger number;
+@property (nonatomic) NSUInteger symbol;
+@property (nonatomic) NSUInteger shading;
+@property (nonatomic) NSUInteger color;
+
+@end
